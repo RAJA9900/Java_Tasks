356df23fc6711c741b25057af010ba2cbaa61dc5
RAJA@9900 <raja9900ganesh@gmail.com>
2021-07-30 07:17:43 +0530

    Tasks

--- july_29/src/july_29/Arraytask3.java
+++ july_29/src/july_29/Arraytask3.java
@@ -4,6 +4,42 @@ public class Arraytask3 {
 
 	public static void main(String args[])
 	{
-		int ar1[]= {101,323,202,2002};
+		int ar[]= {101,234,567,323,202,2002};
+		//int arr[]= {};
+		int a=0;
+		//System.out.println(checkpalindrome(606));
+		for(int i=0;i<ar.length;i++)
+		{
+			if(checkpalindrome(ar[i]))
+			{
+				//System.out.println(ar[i]+" ");
+				a= ar[i];	
+			}
+				
+		
+		System.out.println(a);
+		}	
 	}
+
+    public static boolean checkpalindrome(int n)
+    {
+    	int k=0,n1;
+    	int n2 =n;
+    	while(n2!=0)
+    	{
+    		n1=n2%10;
+    		k=(k*10)+n1;
+    		n2=n2/10;
+    	}
+    	//System.out.println(n);
+    	if(n==k)
+    	{
+    		return true;
+    	}
+    	else
+    	{
+    	    return false;
+    	}
+    	
+    }
 }
